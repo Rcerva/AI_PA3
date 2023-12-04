35066d2c71f7803c52f2f2b4570fa1df8aa6d3dd
RCerva21 <rscervantez@miners.utep.edu>
2023-12-03 17:46:47 -0700

    cleaned Print Statements

--- Application/Main.java
+++ Application/Main.java
@@ -5,22 +5,24 @@ import Algorithms.ValueIteration.ValueIteration;
 
 public class Main {
   public static void main(String[] args) {
+    System.out.println("\n");
     //Monte Carlo Search
     System.out.println("--------MonteCarloSearch--------");
     MonteCarlo mc = new MonteCarlo();
     mc.simulate();
-    System.out.println();
+    System.out.println("\n\n\n");
 
     //Value Iteration Search
     System.out.println("--------ValueIterationSearch--------");
     ValueIteration vi = new ValueIteration();
     vi.valueIterate();
-    System.out.println();
+    System.out.println("\n\n\n");
 
     //Q-Learning Search
     System.out.println("--------QLearningSearch--------");
     QLearning ql = new QLearning();
     ql.runQLearning();
-    System.out.println();
+    System.out.println("\n");
+
   }
 }
